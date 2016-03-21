06bb101df54e8d6368a2183efa7e146f7a074167
Aileen-Lopez <aileen.lopez@ms223.org>
2016-03-21 08:25:59 -0400

    Create March21DoNow.py

---
+++ March21DoNow.py
@@ -0,0 +1,11 @@
+import random
+random.randint(0, 8)
+random.randint(0, 8)
+print(random.randint(0, 3))
+print(random.randint(0, 3))
+print(random.randint(0, 3))
+
+# What Randint does is it accepts two numbers, a lowest and a highest number.
+# The values of 0 and 3 gives us the number 0, 3, and 0.
+# I Changed the numbers to (0,8) and the results where 1 0 1.
+# The difference between Randint and print is that print will continue giving a 3, while Randint stays the same of changes.
