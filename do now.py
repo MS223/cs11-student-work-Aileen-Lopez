d01cf3d6ba2d41180bd182df5cf971a1ee0c51ff
Aileen-Lopez <aileen.lopez@ms223.org>
2016-04-01 08:10:08 -0400

    Create do now.py

---
+++ do now.py
@@ -0,0 +1,24 @@
+a = 0
+while a< 100:
+	print a
+"""
+Prediction:What will happen is that, it will start from 1, and hgo all the way up to 99.
+Observation: It continued to give infinite zero's.
+ """
+
+a = 0
+while a < 100:
+	a = a + 1
+	print a
+"""
+Prediction:What i think will happen is, same as before but it will add +1.
+Observation: It counted from 1 to 100
+ """
+
+a = raw_input("Would you like to quit: ")
+while a != "y":
+	a = raw_input("Would you like to quit: ")
+"""
+Prediction:What will happen is that, it will print out "would you like to quit."
+Observation:It continues to ask "Would you like to quit?"
+ """
