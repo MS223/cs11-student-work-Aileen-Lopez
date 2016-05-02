106c37367aa176d5ed6c4b3288b84e78197cd2e6
Aileen-Lopez <aileen.lopez@ms223.org>
2016-05-02 08:45:35 -0400

    Create May_2_Quiz.py

---
+++ May_2_Quiz.py
@@ -0,0 +1,16 @@
+#1
+def max_value(integer):
+  for x in range(1,integer+1):
+    print x
+print range(1,8)
+
+#2
+list1 = [4,5,15,11,23,42]
+list2 = [1,8,7,16,7,35]
+
+def compare_lists(list1,list1):
+    for x in list1:
+        if x > list2[list1.index(x)]:
+            print x
+        else:
+            print list2[list1.index(x)]
