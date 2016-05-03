6a835fe320cb4d1c68a436e3987439a0ed892e68
Aileen-Lopez <aileen.lopez@ms223.org>
2016-05-03 09:25:24 -0400

    Update de_vwl.py

--- de_vwl.py
+++ de_vwl.py
@@ -1,7 +1,7 @@
+list = ['a', 'e', 'i', 'o', 'u']
 def d_vwl(some_string):
     for n in some_string:
-        if n != "a" and n != "e" and n != "i" and n != "o" and n != "u":
-            return n
-
-print d_vwl("Hello")
-#No se
+        if n in list:
+            some_string = some_string.replace(n,"")
+    print some_string
+d_vwl("Hello")
