9b26622508a15ba3f8cfd50f99ff41062acd5aa3
Aileen-Lopez <aileen.lopez@ms223.org>
2016-05-06 08:38:28 -0400

    Create getting_loopy.py

---
+++ getting_loopy.py
@@ -0,0 +1,12 @@
+#def fruit_pluralizer(list_of_strings):
+#     if list_of_strings ==
+# fruit_list = ['apple', 'berry', 'melon']
+# print("Single Fruit: " + str(fruit_list))
+# fruit_pluralizer(fruit_list)
+# print("No longer single Fruit: " + str(fruit_list))
+
+fruit = raw_input("give me a fruit")
+    if fruit[len(fruit)- 1] + 'ies'
+else:
+    print fruit[len(fruit)] + 's' #dont chop off last letter if its not 'y'
+# I dont know
