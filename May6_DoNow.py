273223e1def8965f29664acc8678c63a755791d3
Aileen-Lopez <aileen.lopez@ms223.org>
2016-05-09 08:40:09 -0400

    Create May6_DoNow.py

---
+++ May6_DoNow.py
@@ -0,0 +1,11 @@
+print "hello"[1:]
+#simply prints out "ello"
+print "hello"[:4]
+#simply prints out "hell"
+print "hello"[2:4]
+#Simply prints out "ll"
+
+print "GitHub"[1:3]
+print "Hamburger"[3:]
+print "dongle"[1:3]
+print "Snapchat"[1:4]
