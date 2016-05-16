0e2719cb5982394dd389edea6bc717c4d4ae7a1e
Aileen-Lopez <aileen.lopez@ms223.org>
2016-05-16 08:43:44 -0400

    Create Dictionary_Lab.py

---
+++ Dictionary_Lab.py
@@ -0,0 +1,8 @@
+user_profile = {'name':'Aileen' ,
+                'school': 'Hs/Ms223',
+                'birthday': 'April 9,1999',
+                'email': 'aileen.lopez@ms223.org',
+                'username': 'Panda123',
+                'number of siblings': 2,
+                "fav TV show": 'Hannah Montana',
+                'fav book':'Maximum Ride'}
