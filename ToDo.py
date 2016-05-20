68f0a8a9ce820da3278bbb8bcdeb6cb991e44db1
Aileen-Lopez <aileen.lopez@ms223.org>
2016-05-20 08:42:58 -0400

    Create ToDo.py

---
+++ ToDo.py
@@ -0,0 +1,14 @@
+action = raw_input("What would you like to do?")
+day = raw_input("What day?")
+empty_dictionary = {'Monday':[],
+                    'Tuesday':[],
+                    'Wednesday':[],
+                    'Thursday':[],
+                    'Friday':[],
+                    'Saturday':[],
+                    'Sunday':[]}
+def add(empty_dictionary):
+
+days_of_week[day] = action
+print days_of week
+#I dont know what's next
