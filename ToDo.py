b3c1a4af26c32e55d792367078579bf4c607f3ad
Aileen-Lopez <aileen.lopez@ms223.org>
2016-05-24 08:45:54 -0400

    Update ToDo.py

--- ToDo.py
+++ ToDo.py
@@ -1,5 +1,3 @@
-action = raw_input("What would you like to do?")
-day = raw_input("What day?")
 empty_dictionary = {'Monday':[],
                     'Tuesday':[],
                     'Wednesday':[],
@@ -10,21 +8,21 @@ empty_dictionary = {'Monday':[],
 def add(action,day):
     # loop this question...
         #append the action to the list value of the day keys
-        some_dictionary[day].append(action)
+        empty_dictionary[day].append(action)
     #until you...
     # I need an option to call choice()
 
 def get(day):
-    print some_dictionary[day]
+    print empty_dictionary[day]
     # I need an option to call choice()
 
 def choice():
     user_choice = raw_input("How can I help you?")
-    # if they choose 'add' call add()
-    # if they choose 'get' call get()
-add("something", "monday")
-
-get("monday")
-
-# days_of_week[day] = action
-# print days_of week
+    if user_choice == 'add':
+        action = raw_input("What would you like to do?")
+        day = raw_input("What day?").capitalize()
+        add(action,day)
+    elif user_choice == 'get':
+        action = raw_input("What would you like to do?")
+        day = raw_input('What day?').capitalize()
+print empty_dictionary
