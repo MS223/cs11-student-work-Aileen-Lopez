5618aca3b3573d43e498e43e9a9074f0264d2f7f
Aileen-Lopez <aileen.lopez@ms223.org>
2016-05-31 09:26:54 -0400

    Create Method_To_The_Madness.py

---
+++ Method_To_The_Madness.py
@@ -0,0 +1,15 @@
+class Time(object):
+    def __init__(self, hour, minute, second):
+        self.hour = hour
+        self.minute = minute
+        self.second = second
+    def __str__(self):
+        return str(self.hour) + ":" + str(self.minute) + ":" + str(self.second)
+    def __add__(self, other):
+
+
+
+time1 = time(5,32,0)
+time2 = time()
+
+print time1
