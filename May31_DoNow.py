66fb1d3d910b2db4ed0b95650649a2e5528ca566
Aileen-Lopez <aileen.lopez@ms223.org>
2016-05-31 08:06:14 -0400

    Create May31_DoNow.py

---
+++ May31_DoNow.py
@@ -0,0 +1,15 @@
+class Time(object):
+ def __init__(self, hour, minute, second):
+     self.hour = hour
+     self.minute = minute
+     self.second = second
+
+time1 = Time(5, 32, 0)
+time2 = Time(23, 11, 11)
+
+print time1
+print time2
+
+#The difference is that at the "c91d0 and c9190"
+#You add hour minute and second to time 1.
+#I don't know how to add them.
